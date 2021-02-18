42fbb941f054d312e8f2ff54d3311303ebf80551
Adrian <adrianabadjacob@gmail.com>
2021-02-18 17:26:27 +0100

    Footer y Header modificado
    Arreglos para el header y footer.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,12 @@
 </main>
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
-      <span class="text-muted">Proyecto de Melania , Aitor y Adrian</span>
+      <span class="text-muted">Trabajo de Melania, Aitor y Adrian</span>
     </div>
   </footer>
-</body></html>
+  
+  
+      <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  
+  </body></html>
 
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,14 +1,18 @@
-<html lang="en"><head>
+<html lang="en" class="h-100">
+    <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.80.0">
-    <title>Navbar Template · Bootstrap v5.0</title>
+    <title>Sticky Footer Navbar Template · Bootstrap v5.0</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbars/">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
 
-    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
@@ -44,36 +48,36 @@
 
     
     <!-- Custom styles for this template -->
-    <link href="navbar.css" rel="stylesheet">
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
   </head>
-  <body>
+  <body class="d-flex flex-column h-100">
     
-<main>
-
-<nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+<header>
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-    
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="Home">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExample04">
+      <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="home">Home</a>
+            <a class="nav-link active" aria-current="page" href="migraciones">Migraciones</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="personajes">Personajes</a>
+            <a class="nav-link" href="seeders">Seeders</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="eventos">Eventos</a>
+            <a class="nav-link" href="mail">Mail</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="sugerencias">Sugerencias</a>
-          </li>
-         
+          
         </ul>
-        
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </nav>
+</header>
