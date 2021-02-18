952f88fcc9e5dffe4a4bc33ce16c4f497d5ba3d5
Adrian <adrianabadjacob@gmail.com>
2021-02-18 19:05:18 +0100

    Update eventos.blade.php
    Se han introducido todos los eventos a la pagina

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -2,8 +2,9 @@
 <main>
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
-            <h1 class="display-4 fw-normal">Title</h1>
-            <p class="lead fw-normal">Context</p>
+            <h1 class="display-4 fw-normal">Tower defense event!</h1>
+            <p class="lead fw-normal">2021/02/11 04:00:00 - 2021/02/26 03:59:59</p>
+            <p class="lead fw-normal">El evento estara disponible durante el login y los jugadores recibiran 10 'Intertwined Fate' durante los proximos 7 dias</p>
             
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
@@ -12,8 +13,9 @@
     
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
-            <h1 class="display-4 fw-normal">Title</h1>
-            <p class="lead fw-normal">Context</p>
+            <h1 class="display-4 fw-normal">Baptism of Song Event!</h1>
+            <p class="lead fw-normal">2021/03/15 04:00:00 - 2021/03/20 03:59:59</p>
+            <p class="lead fw-normal">Acepta la mision especial que garantiza el personaje Barbara</p>
             
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
@@ -22,9 +24,10 @@
 
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
-            <h1 class="display-4 fw-normal">Title</h1>
-            <p class="lead fw-normal">Context</p>
-            
+            <h1 class="display-4 fw-normal">Spiral Abyss Event</h1>
+            <p class="lead fw-normal">2021/03/26 04:00:00 - 2021/04/11 03:59:59</p>
+            <p class="lead fw-normal">Una vez completes la mazmorra del evento desbloquears un personaje secreto</p>
+
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
         <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
