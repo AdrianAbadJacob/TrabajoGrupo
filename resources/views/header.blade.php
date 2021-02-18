5dd9a56059b8de0b63b33b0289b52fa11f2afe13
Adrian <adrianabadjacob@gmail.com>
2021-02-18 16:57:12 +0100

    Footer modificado
    Añadido contenido a footer.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -52,7 +52,7 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Expand at md</a>
+    
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -60,22 +60,20 @@
       <div class="collapse navbar-collapse" id="navbarsExample04">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="home">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Personajes</a>
+            <a class="nav-link" href="personajes">Personajes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Eventos</a>
+            <a class="nav-link" href="eventos">Eventos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Sugerencias</a>
+            <a class="nav-link" href="sugerencias">Sugerencias</a>
           </li>
          
         </ul>
-        <form>
-          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-        </form>
+        
       </div>
     </div>
   </nav>
